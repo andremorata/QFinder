b08c967a0597d6baf18f7c528a59a57d3ea769b7
andre.morata <andre.morata@PMWKS13>
2016-10-15 17:55:28 -0300

    Help text improoved

--- QFinder/Helpers/SpecialCommands.cs
+++ QFinder/Helpers/SpecialCommands.cs
@@ -15,19 +15,18 @@ namespace QFinder.Helpers
             {
                 case "help":
                     StringBuilder str = new StringBuilder();
-                    str.AppendLine("\r\n\r\n");
                     str.AppendLine("Commands:");
-                    str.AppendLine("Help: This dialog;");
-                    str.AppendLine("Exit: Terminates the app instance;");
-                    str.AppendLine("EditIndexPaths or EID: Shows the index paths manager interface;");
-                    str.AppendLine("EditIndexSchedule or EIS: Shows the automatic index scheduler interface;");
-                    str.AppendLine("BuildIndex: Gets confirmation to start mapping or re-mapping files;");
+                    str.AppendLine("\r\nHelp: This dialog;");
+                    str.AppendLine("\r\nExit: Terminates the app instance;");
+                    str.AppendLine("\r\nEditIndexPaths or EID: Shows the index paths manager interface;");
+                    str.AppendLine("\r\nEditIndexSchedule or EIS: Shows the automatic index scheduler interface;");
+                    str.AppendLine("\r\nBuildIndex: Gets confirmation to start mapping or re-mapping files;");
                     str.AppendLine("\r\n\r\n");
                     str.AppendLine("Query Options:");
-                    str.AppendLine("To query files by its name, simply type few pieces of the file name between spaces. Ex: grandma's recipe;");
-                    str.AppendLine("To query files of a specific type, use the extensions that you want to filter followed by an '>' and then the terms to filter the file name. EX: 'docx>grandma's recipe';");
-                    str.AppendLine("To query files stored in a specific folder or path, type part of the path followed by '/' and then type the pieces of the file that you want to find. EX: recipes/grandma's cheesecake");
-                    str.AppendLine("You can also mix the query filters like 'docx>recipes/grandma's cheesecake';");
+                    str.AppendLine("\r\nTo query files by its name, simply type few pieces of the file name between spaces. Ex: grandma's recipe;");
+                    str.AppendLine("\r\nTo query files of a specific type, use the extensions that you want to filter followed by an '>' and then the terms to filter the file name. EX: 'docx>grandma's recipe';");
+                    str.AppendLine("\r\nTo query files stored in a specific folder or path, type part of the path followed by '/' and then type the pieces of the file that you want to find. EX: recipes/grandma's cheesecake");
+                    str.AppendLine("\r\nYou can also mix the query filters like 'docx>recipes/grandma's cheesecake';");
                     MessageBox.Show(str.ToString(), "QFinder - Help");
                     return true;
                 case "exit":
