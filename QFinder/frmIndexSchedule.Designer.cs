6985dcf8881c8817a41364e7b8668587dec7035e
Andre Morata <andre@playmoresoftware.com>
2019-04-03 10:55:36 -0300

    Enabled per monitor DPI awareness

--- QFinder/frmIndexSchedule.Designer.cs
+++ QFinder/frmIndexSchedule.Designer.cs
@@ -33,14 +33,16 @@
             this.ddlType = new System.Windows.Forms.ComboBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.ckEnable = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.txtValue)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtValue
             // 
             this.txtValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtValue.Font = new System.Drawing.Font("Ubuntu Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtValue.Location = new System.Drawing.Point(12, 44);
+            this.txtValue.Font = new System.Drawing.Font("Ubuntu Light", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValue.Location = new System.Drawing.Point(3, 51);
             this.txtValue.Maximum = new decimal(new int[] {
             9999999,
             0,
@@ -52,7 +54,7 @@
             0,
             0});
             this.txtValue.Name = "txtValue";
-            this.txtValue.Size = new System.Drawing.Size(120, 30);
+            this.txtValue.Size = new System.Drawing.Size(120, 32);
             this.txtValue.TabIndex = 1;
             this.txtValue.Value = new decimal(new int[] {
             1,
@@ -69,44 +71,63 @@
             "Minutes",
             "Days",
             "Weeks"});
-            this.ddlType.Location = new System.Drawing.Point(138, 44);
+            this.ddlType.Location = new System.Drawing.Point(129, 51);
             this.ddlType.Name = "ddlType";
-            this.ddlType.Size = new System.Drawing.Size(121, 30);
+            this.ddlType.Size = new System.Drawing.Size(121, 32);
             this.ddlType.TabIndex = 2;
             // 
             // btnSave
             // 
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSave.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(265, 46);
+            this.btnSave.Location = new System.Drawing.Point(256, 51);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(95, 26);
+            this.btnSave.Size = new System.Drawing.Size(102, 34);
             this.btnSave.TabIndex = 5;
             this.btnSave.Text = "Save";
+            this.btnSave.UseCompatibleTextRendering = true;
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // ckEnable
             // 
+            this.ckEnable.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.ckEnable.AutoSize = true;
-            this.ckEnable.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.SetColumnSpan(this.ckEnable, 3);
+            this.ckEnable.Location = new System.Drawing.Point(3, 10);
             this.ckEnable.Name = "ckEnable";
-            this.ckEnable.Size = new System.Drawing.Size(174, 26);
+            this.ckEnable.Size = new System.Drawing.Size(212, 28);
             this.ckEnable.TabIndex = 6;
             this.ckEnable.Text = "Refresh index every";
             this.ckEnable.UseVisualStyleBackColor = true;
             this.ckEnable.CheckedChanged += new System.EventHandler(this.ckEnable_CheckedChanged);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.ckEnable, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSave, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtValue, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ddlType, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(431, 97);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
             // frmIndexSchedule
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(377, 91);
-            this.Controls.Add(this.ckEnable);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.ddlType);
-            this.Controls.Add(this.txtValue);
+            this.ClientSize = new System.Drawing.Size(431, 97);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Ubuntu Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -119,8 +140,9 @@
             this.Text = "Manage Index Schedule";
             this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.txtValue)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -130,5 +152,6 @@
         private System.Windows.Forms.ComboBox ddlType;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.CheckBox ckEnable;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- QFinder/frmManageIndexFolders.Designer.cs
+++ QFinder/frmManageIndexFolders.Designer.cs
@@ -31,7 +31,7 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmIndexSchedule));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmManageIndexFolders));
             this.grd = new System.Windows.Forms.DataGridView();
             this.dlgFolderBrowse = new System.Windows.Forms.FolderBrowserDialog();
             this.label1 = new System.Windows.Forms.Label();
@@ -40,7 +40,11 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.grd)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // grd
@@ -61,6 +65,7 @@
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.grd.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.grd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.grd, 3);
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.Black;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Ubuntu Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -69,10 +74,10 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.grd.DefaultCellStyle = dataGridViewCellStyle2;
-            this.grd.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.grd.Dock = System.Windows.Forms.DockStyle.Fill;
             this.grd.EnableHeadersVisualStyles = false;
             this.grd.GridColor = System.Drawing.Color.Black;
-            this.grd.Location = new System.Drawing.Point(0, 81);
+            this.grd.Location = new System.Drawing.Point(4, 81);
             this.grd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.grd.MultiSelect = false;
             this.grd.Name = "grd";
@@ -88,7 +93,7 @@
             this.grd.RowHeadersWidth = 15;
             this.grd.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.grd.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.grd.Size = new System.Drawing.Size(653, 444);
+            this.grd.Size = new System.Drawing.Size(565, 363);
             this.grd.TabIndex = 0;
             this.grd.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.grd_CellBeginEdit);
             this.grd.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grd_CellDoubleClick);
@@ -98,10 +103,11 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(3, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 22);
+            this.label1.Size = new System.Drawing.Size(55, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Path:";
             // 
@@ -109,21 +115,24 @@
             // 
             this.txtPath.BackColor = System.Drawing.Color.Black;
             this.txtPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPath.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtPath.ForeColor = System.Drawing.Color.White;
-            this.txtPath.Location = new System.Drawing.Point(63, 8);
+            this.txtPath.Location = new System.Drawing.Point(64, 3);
             this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(477, 26);
+            this.txtPath.Size = new System.Drawing.Size(405, 30);
             this.txtPath.TabIndex = 2;
             // 
             // btnBrowse
             // 
+            this.btnBrowse.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBrowse.Font = new System.Drawing.Font("Ubuntu", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBrowse.Location = new System.Drawing.Point(546, 8);
+            this.btnBrowse.Location = new System.Drawing.Point(475, 5);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(95, 26);
             this.btnBrowse.TabIndex = 3;
             this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseCompatibleTextRendering = true;
             this.btnBrowse.UseVisualStyleBackColor = true;
             this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
@@ -131,11 +140,12 @@
             // 
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSave.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(344, 47);
+            this.btnSave.Location = new System.Drawing.Point(267, 3);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(95, 26);
             this.btnSave.TabIndex = 4;
             this.btnSave.Text = "Save";
+            this.btnSave.UseCompatibleTextRendering = true;
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
@@ -143,11 +153,12 @@
             // 
             this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRemove.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemove.Location = new System.Drawing.Point(445, 47);
+            this.btnRemove.Location = new System.Drawing.Point(368, 3);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(95, 26);
             this.btnRemove.TabIndex = 5;
             this.btnRemove.Text = "Remove";
+            this.btnRemove.UseCompatibleTextRendering = true;
             this.btnRemove.UseVisualStyleBackColor = true;
             this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
@@ -155,27 +166,56 @@
             // 
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancel.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(546, 47);
+            this.btnCancel.Location = new System.Drawing.Point(469, 3);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(95, 26);
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseCompatibleTextRendering = true;
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.btnBrowse, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.grd, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtPath, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(573, 449);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 3);
+            this.flowLayoutPanel1.Controls.Add(this.btnCancel);
+            this.flowLayoutPanel1.Controls.Add(this.btnRemove);
+            this.flowLayoutPanel1.Controls.Add(this.btnSave);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 39);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(567, 34);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
             // frmManageIndexFolders
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(653, 525);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnRemove);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.btnBrowse);
-            this.Controls.Add(this.txtPath);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.grd);
+            this.ClientSize = new System.Drawing.Size(573, 449);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Ubuntu Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -188,8 +228,10 @@
             this.Text = "Manage Index Folders";
             this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.grd)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -203,5 +245,7 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
